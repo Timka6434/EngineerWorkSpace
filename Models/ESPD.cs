7f8144ab00974d815104114778b43ee4e7d1a357
Timka6434 <timka6434@mail.ru>
2024-07-17 16:35:26 +0500

    Add-SomeFunctional
    Create Functional Check EthernetAccess

---
+++ Models/ESPD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineerWorkSplace.Models
+{
+    internal class ESPD
+    {
+        public bool ServiceStatus { get; set; }
+        public bool EthernetAccess {  get; set; }
+        public int PingESPD { get; set; }
+        public bool ProxyAccess { get; set; }
+        public int PingProxy { get; set; }
+    }
+}
---
+++ ViewModels/ServiceESPD_VM.cs
@@ -0,0 +1,66 @@
+﻿using EngineerWorkSplace.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace EngineerWorkSplace.ViewModels
+{
+    internal class ServiceESPD_VM : BaseViewModel
+    {
+        private ESPD espd;
+
+        public ServiceESPD_VM()
+        {
+            espd = new ESPD();
+            StartMonitoring();
+        }
+
+        public bool EthernetAccess
+        {
+            get => espd.EthernetAccess;
+
+            set
+            {
+                if (espd.EthernetAccess != value)
+                {
+                    espd.EthernetAccess = value;
+                    OnPropertyChanged(nameof(EthernetAccess));
+                    Console.WriteLine(EthernetAccess);
+                }
+            }
+        }
+
+        public async Task<bool> CheckEthernetAccessAsync()
+        {
+            try
+            {
+                using(var ping = new Ping())
+                {
+                    var reply = await ping.SendPingAsync("8.8.8.8", 1000);
+                    return reply.Status == IPStatus.Success;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ping failed: {ex.Message}"); // Вывод сообщения об ошибке
+                return false;
+            }
+        }
+
+        private async void StartMonitoring()
+        {
+            while (true)
+            {
+                EthernetAccess = await CheckEthernetAccessAsync();
+                await Task.Delay(5000); // Повторная проверка каждые 5 секунд
+            }
+        }
+
+
+        Ы
+    }
+}
