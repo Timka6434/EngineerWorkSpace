9efade92eff46c500e47acfbc54dbfcad1d03e60
Timka6434 <timka6434@mail.ru>
2024-07-17 17:10:13 +0500

    Add-NewBindings

--- ViewModels/ServiceESPD_VM.cs
+++ ViewModels/ServiceESPD_VM.cs
@@ -1,35 +1,99 @@
 ﻿using EngineerWorkSplace.Models;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
 namespace EngineerWorkSplace.ViewModels
 {
     internal class ServiceESPD_VM : BaseViewModel
     {
         private ESPD espd;
+        private HttpClient httpClient;
+        private HttpClient proxyClient;
 
         public ServiceESPD_VM()
         {
             espd = new ESPD();
+            httpClient = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
+            var httpClientHandler = new HttpClientHandler
+            {
+                Proxy = new System.Net.WebProxy("http://10.0.86.52:3128"),
+                UseProxy = true
+            };
+            proxyClient = new HttpClient(httpClientHandler)
+            {
+                Timeout = TimeSpan.FromSeconds(5)
+            };
+
             StartMonitoring();
+        }
+
+        public bool ServiceStatus
+        {
+            get => espd.ServiceStatus;
+            set
+            {
+                if (espd.ServiceStatus != value)
+                {
+                    espd.ServiceStatus = value;
+                    OnPropertyChanged(nameof(ServiceStatus));
+                }
+            }
         }
 
         public bool EthernetAccess
         {
             get => espd.EthernetAccess;
-
             set
             {
                 if (espd.EthernetAccess != value)
                 {
                     espd.EthernetAccess = value;
                     OnPropertyChanged(nameof(EthernetAccess));
-                    Console.WriteLine(EthernetAccess);
+                }
+            }
+        }
+
+        public int PingESPD
+        {
+            get => espd.PingESPD;
+            set
+            {
+                if (espd.PingESPD != value)
+                {
+                    espd.PingESPD = value;
+                    OnPropertyChanged(nameof(PingESPD));
+                }
+            }
+        }
+
+        public bool ProxyAccess
+        {
+            get => espd.ProxyAccess;
+            set
+            {
+                if (espd.ProxyAccess != value)
+                {
+                    espd.ProxyAccess = value;
+                    OnPropertyChanged(nameof(ProxyAccess));
+                }
+            }
+        }
+
+        public int PingProxy
+        {
+            get => espd.PingProxy;
+            set
+            {
+                if (espd.PingProxy != value)
+                {
+                    espd.PingProxy = value;
+                    OnPropertyChanged(nameof(PingProxy));
                 }
             }
         }
@@ -38,17 +102,68 @@ namespace EngineerWorkSplace.ViewModels
         {
             try
             {
-                using(var ping = new Ping())
+                var response = await httpClient.GetAsync("http://www.google.com");
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"HTTP request failed: {ex.Message}");
+                return false;
+            }
+        }
+
+        public async Task<bool> CheckProxyAccessAsync()
+        {
+            try
+            {
+                var response = await proxyClient.GetAsync("http://www.google.com");
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"HTTP request via proxy failed: {ex.Message}");
+                return false;
+            }
+        }
+
+        public async Task<int> CheckPingEthernetAsync()
+        {
+            try
+            {
+                using (var ping = new Ping())
                 {
                     var reply = await ping.SendPingAsync("8.8.8.8", 1000);
-                    return reply.Status == IPStatus.Success;
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        return (int)reply.RoundtripTime;
+                    }
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Ping failed: {ex.Message}"); // Вывод сообщения об ошибке
-                return false;
+                Console.WriteLine($"Ping failed: {ex.Message}");
             }
+            return -1; // Возвращает -1 в случае ошибки
+        }
+
+        public async Task<int> CheckPingProxyAsync()
+        {
+            try
+            {
+                using (var ping = new Ping())
+                {
+                    var reply = await ping.SendPingAsync("10.0.86.52", 1000);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        return (int)reply.RoundtripTime;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ping proxy failed: {ex.Message}");
+            }
+            return -1; // Возвращает -1 в случае ошибки
         }
 
         private async void StartMonitoring()
@@ -56,11 +171,12 @@ namespace EngineerWorkSplace.ViewModels
             while (true)
             {
                 EthernetAccess = await CheckEthernetAccessAsync();
+                PingESPD = await CheckPingEthernetAsync();
+                ProxyAccess = await CheckProxyAccessAsync();
+                PingProxy = await CheckPingProxyAsync();
+
                 await Task.Delay(5000); // Повторная проверка каждые 5 секунд
             }
         }
-
-
-        Ы
     }
 }
